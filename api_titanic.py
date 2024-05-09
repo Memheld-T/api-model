44ad7906bc2d924996bdafbb37808d22c7782806
Memheld-T <thibault.memheld@student-cs.fr>
2024-05-09 16:29:29 +0100

    Update api_titanic.py

--- api_titanic.py
+++ api_titanic.py
@@ -2,20 +2,30 @@ import joblib
 import uvicorn
 from fastapi import FastAPI
 import pandas as pd
-from prometheus_client import make_asgi_app
+from prometheus_client import make_asgi_app, Counter
 
 app = FastAPI()
 metrics_app = make_asgi_app()
 app.mount("/metrics",metrics_app)
 
+survived_counter=Counter("survived","Number of survived passengers")
+not_survived_counter=Counter("not_survived","Number of not survived passengers")
+
+# Load model
+titanic_model = joblib.load("model_titanic.joblib")
+
 @app.post("/titanic")
 def prediction_api(pclass: int, sex: int, age: int) -> bool:
-    # Load model
-    titanic_model = joblib.load("model_titanic.joblib")
 
     # predict
     x = [pclass, sex, age]
     prediction = titanic_model.predict(pd.DataFrame(x).transpose())
+
+    survived=prediction[0] == 1
+    if survived:
+        survived_counter.inc()
+    else:
+        not_survived_counter.inc()
 
     return prediction[0] == 1
 
